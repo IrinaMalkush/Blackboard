4c97d42fca4661107470ecef279101fd57eb6665
irina <irinamalkush@gmail.com>
2023-10-18 00:37:33 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        the blackboard app for Irina Dolgoniuk master's diploma
+      </header>
+    </div>
+  );
+}
+
+export default App;
